2758778d5c0930d9988740f915e3308146dc2be5
marciopalves <marciopalves89@gmail.com>
2023-05-04 07:25:20 -0300

    Criação do projeto para o Canal OverWrite-X E-mails profissionais com nodemailer

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # NodeEmails
 Api Node utilizando o Nodemailer para geração de e-mails profissionais.
+
+Crie o arquivo .env com as informações
+
+HOST_MAIL=
+HOST_PORT=
+HOST_USER=
+HOST_PASS=
+
+Link mailtrap onde pode pegar estes dados como exemplo para rodar a aplicação 
+https://mailtrap.io/home
+
+# Rode o comando node Server.js  -- Para roda o programa
+
---
+++ server.js
@@ -0,0 +1,77 @@
+require('dotenv').config();
+const nodemailer = require("nodemailer");
+const express = require("express");
+const ejs = require("ejs");
+const port = 3001;
+
+const app = express();
+
+
+app.get("/sendemail", async (req, res)=>{
+
+    //transport
+    var transport = nodemailer.createTransport({
+        host: process.env.HOST_MAIL,
+        port: process.env.HOST_PORT,
+        auth: {
+          user: process.env.HOST_USER,
+          pass: process.env.HOST_PASS
+        }
+      });
+
+
+    //Config Email 
+    let message = await transport.sendMail({
+        from: '"Pessoa Teste" <pessoa@teste.com>',
+        to:"teste@teste.com",
+        subject:"Email Teste",
+        text: "Email Teste utilizando Nodemailer",
+        html: "<p><strong>Test Nodemailer</strong> Test Nodemailer With mail trap </p>"
+    })
+
+
+    res.send("Email enviado com sucesso");
+});
+
+
+app.get("/sendhtml", async (req, res)=>{
+    
+    ejs.renderFile(__dirname+"/email.ejs", function(err, data) {
+        if(err){
+          console.log(err);
+        } else{
+              //transporter
+              var transport = nodemailer.createTransport({
+                host: process.env.HOST_MAIL,
+                port: process.env.HOST_PORT,
+                auth: {
+                  user: process.env.HOST_USER,
+                  pass: process.env.HOST_PASS
+                }
+              }); 
+  
+              let mailOptions = {
+                from: '"Pessoa Teste" pessoa@teste.com',
+                to:   "pessoa@teste.com",
+                subject: "E-mail com Html",
+                html: data
+              }
+  
+              transport.sendMail(mailOptions, function(err, inf){
+                  if(err){
+                    console.log(err);
+                  } else {
+                    console.log("E-mail com html enviado com sucesso!");
+                  }
+              });
+  
+        }  
+    });
+    
+    res.send("E-mail com html enviado com sucesso!");
+  });
+
+
+app.listen(port, ()=>{
+    console.log(`Rodando na porta ${port}...`)
+});
